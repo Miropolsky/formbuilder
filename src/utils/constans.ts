785be31a594b42aeffc0614811fff413de591885
Miropolsky <Miropolskysk@yandex.ru>
2024-09-13 11:39:15 +0500

    update translate

--- src/utils/constans.ts
+++ src/utils/constans.ts
@@ -116,8 +116,30 @@ export const translations = {
         Blur: "размытие ",
         "Select the type of applying mask.": "Выберите тип применяемой маски.",
         Data: "Данные",
+        "Add Another": "Добавить еще один",
+        "Unique Options": "Уникальные возможности",
         "Default Value": "Значение по умолчанию",
         "Multiple Values": "Несколько значений",
+        "Allows multiple values to be entered for this field.":
+            "Позволяет вводить несколько значений для этого поля.",
+        "The source to use for the select data. Values lets you provide your own values and labels. JSON lets you provide raw JSON data. URL lets you provide a URL to retrieve the JSON data from.":
+            "Источник, который будет использоваться для выбранных данных. Values позволяет указать ваши собственные значения и метки. JSON позволяет предоставить необработанные данные в формате JSON. URL позволяет указать URL-адрес для получения данных в формате JSON",
+        "The Default Value will be the value for this field, before user interaction. Having a default value will override the placeholder text.":
+            "Значением по умолчанию будет значение для этого поля до взаимодействия с пользователем. Значение по умолчанию переопределит текст-заполнитель.",
+        "Values to use as the data source. Labels are shown in the select field. Values are the corresponding values saved with the submission.":
+            "Значения, которые будут использоваться в качестве источника данных. В поле выбора отображаются метки. Значения - это соответствующие значения, сохраненные при отправке.",
+        "The type to store the data. If you select something other than autotype, it will force it to that type.":
+            "Тип, в котором будут храниться данные. Если вы выберете что-то другое, кроме автоматического ввода, это принудительно приведет к этому типу.",
+        "Path to the select option id.":
+            "Путь к идентификатору выбранного параметра.",
+        "The HTML template for the result data items.":
+            "HTML-шаблон для результирующих элементов данных.",
+        "Refresh Options On": "Обновить параметры на",
+        "Refresh Options On Blur": "Обновить параметры Размытия",
+        "Refresh data when another field is blured.":
+            "Обновляйте данные, когда другое поле размыто.",
+        "Refresh data when another field changes.":
+            "Обновляйте данные при изменении другого поля.",
         "Position for the label for this field.": "Позиция названия поля",
         "Left (Left-aligned)": "Слева (по левому краю)",
         "Left (Right-aligned)": "Слева (по правому краю)",
@@ -181,8 +203,13 @@ export const translations = {
             "Смешанный (допускается использование верхнего и нижнего регистра)",
         Uppercase: "Верхний регистр",
         Lowercase: "Нижний регистр",
+
+        "Display only unique dropdown options.":
+            "Отображать только уникальные выпадающие варианты.",
         "Truncate Multiple Spaces": "Удаление нескольких пробелов",
         "Redraw On": "Перерисовать при",
+        "Select the type of widget you'd like to use.":
+            "Выберите тип виджета, который вы хотели бы использовать.",
         "Any change": "Какие-либо изменения",
         "Minimum Length": "Минимальная длина",
         "The minimum length requirement this field must meet.":
@@ -205,6 +232,202 @@ export const translations = {
             "Показывать это значение в табличном представлении отправленных данных.",
         "Opens up a modal to edit the value of this component.":
             "Открывает режим модального окна для редактирования значения этого компонента.",
+        "Clear Value On Refresh Options":
+            "Очистить значение при обновлении параметра",
+        "When the Refresh On field is changed, clear this components value.":
+            "Когда поле обновляется, то значение компонента будет изменено",
+        "Enable Static Search": "Включить статический поиск",
+        "When checked, the select dropdown will allow for searching within the static list of items provided.":
+            "Если выбрать этот флаг, то выпадающий список позволит выполнять поиск в предоставленном статическом списке элементов.",
+        "Search Threshold": "Порог поиска",
+        "At what point does the match algorithm give up. A threshold of 0.0 requires a perfect match, a threshold of 1.0 would match anything.":
+            "В какой момент алгоритм сопоставления перестает работать. Пороговое значение 0.0 требует идеального соответствия, пороговое значение 1.0 соответствует чему угодно.",
+        "Read Only Value": "Значение, доступное только для чтения",
+        "Choices.js options": "Choices.js варианты",
+        "A raw JSON object to use as options for the Select component (Choices JS).":
+            "Необработанный объект JSON для использования в качестве параметров для компонента Select (Выбор JS).",
+        "Use exact search": "Использовать точный поиск",
+        "Disables search algorithm threshold.":
+            "Отключает пороговое значение алгоритма поиска.",
+        "Calculate Value on server": "Вычислить значение на сервере",
+        "Checking this will run the calculation on the server. This is useful if you wish to override the values submitted with the calculations performed on the server.":
+            "Если вы установите этот флажок, вычисления будут запущены на сервере. Это полезно, если вы хотите переопределить значения, отправленные с помощью вычислений, выполненных на сервере.",
+        "Allow Manual Override of Calculated Value":
+            "Разрешить ручное изменение расчетного значения",
+        "When checked, this will allow the user to manually override the calculated value.":
+            "Если этот флаг установлен, это позволит пользователю вручную переопределить вычисленное значение.",
+        Key: "Ключ",
+        "This allows control over how many rows are visible in the text area.":
+            "Это позволяет контролировать количество строк, отображаемых в текстовой области.",
+        Editor: "Редактор",
+        "Select the type of WYSIWYG editor to use for this text area.":
+            "Выберите тип WYSIWYG-редактора, который будет использоваться для этой текстовой области.",
+        "Auto Expand": "Автоматическое расширение",
+        "This will make the TextArea auto expand it's height as the user is typing into the area.":
+            "Это приведет к автоматическому увеличению высоты текстовой области по мере того, как пользователь будет вводить текст в эту область.",
+        "Error Label": "Лейбел ошибки",
+        "The label for this field when an error occurs.":
+            "Лейбел для этого поля при возникновении ошибки.",
+        "Inline Layout": "Линейная отрисовка",
+        "Displays the checkboxes/radios horizontally.":
+            "Отображает флаги/радиоприемники горизонтально.",
+        Values: "Значения",
+        Shortcut: "Ярлык",
+        "The shortcut key for this option.": "Ярлык для этой опции.",
+        "Clear Value When Hidden": "Стирать значение когда компонент скрыт",
+        Delimiter: "Разделитель",
+        "What is used to separate the tags.":
+            "Что используется для разделения тегов.",
+        "The maximum amount of tags that can be added. 0 for infinity.":
+            "Максимальное количество тегов, которые можно добавить. 0 означает бесконечность.",
+        "Store As": "Хранить как",
+        "Property Name": "Имя свойства",
+        "The name of this field in the API endpoint.":
+            "Имя этого поля в конечной точке API.",
+        "Field Tags": "Поле тегов",
+        "This component should Display:": "Этот компонент должен отображаться?",
+        "When the form component:": "Когда компонент формы:",
+        "Has the value:": "Имеет значение:",
+        "Advanced Logic": "Дополнительная логика",
+        "Add Logic": "Добавить логику",
+        "Attribute Name": "Имя Аттрибута",
+        "Attribute Value": "Значение Аттрибута",
+        "Add Attribute": "Добавить аттрибут",
+        "Display in Timezone": "Отображать в часовом поясе",
+        "This will display the captured date time in the select timezone.":
+            "Будет отображена записанная дата и время в выбранном часовом поясе",
+        "Use Locale Settings": "Использовать локальные настройки",
+        "Use locale settings to display date and time.":
+            "Использовать региональные настройки для отображения даты и времени.",
+        "The date format for displaying the datetime value.":
+            "Формат даты для отображения значения даты и времени.",
+        "Shortcut Buttons": "",
+        "Enable Date Input": "Включить ввод данных",
+        "Disable specific dates or dates by range":
+            "Отключить определенные даты или диапазоны дат",
+        "Disable weekends": "Отключить выходные дни",
+        "Disable weekdays": "Отключить рабочие дни",
+        "Check to disable weekends":
+            "Установите флажок, чтобы отключить выходные дни",
+        "Check to disable weekdays":
+            "Установите флажок, чтобы отключить рабочие дни",
+        "Enable Time Input": "Включить ввод времени",
+        "Enables time input for this field.":
+            "Включает ввод времени для этого поля.",
+        "Hour Step Size": "Часовой шаг",
+        "Minute Step Size": "Минутный шаг",
+        "The number of minutes to increment/decrement in the time picker.":
+            "Количество минут, которое нужно увеличить/уменьшить в окне выбора времени.",
+        "The number of hours to increment/decrement in the time picker.":
+            "Количество часов, которое нужно увеличить/уменьшить в окне выбора времени.",
+        "Default Date": "Дата по умолчанию",
+        "You can use Moment.js functions to set the default value to a specific date. For example: moment().subtract(10, 'days')": `Вы можете использовать Moment.js функции, чтобы установить значение по умолчанию на определенную дату. Например:
+                момент().вычтите(10, "дни")`,
+        "Use Input to add moment.js for minDate":
+            "Пользовательский ввод для добавления moment.js для minDate",
+        "Enables to use input for moment functions instead of calendar.":
+            "Позволяет использовать входные данные для функций moment вместо календаря.",
+        "Use calendar to set minDate":
+            "Используйте календарь для настройки минимальной даты",
+        "User Input to add moment.js for maxDate":
+            "Вводимые пользователем данные для добавления moment.js для maxDate",
+        "Use calendar to set maxDate":
+            "Используйте календарь для установки максимальной даты",
+        "Enables to use calendar to set date.":
+            "Позволяет использовать календарь для установки даты.",
+        Month: "Месяц",
+        Year: "Год",
+        "Day First": "Первый день",
+        "Type of input": "Тип входных данных",
+        "The placeholder text that will appear when Month field is empty.":
+            "Текст-заполнитель, который будет отображаться, когда поле месяца будет пустым.",
+        "The placeholder text that will appear when Day field is empty.":
+            "Текст-заполнитель, который будет отображаться, когда поле 'День' будет пустым.",
+        "Minimum Year": "Минимальный год",
+        "The minimum year that can be entered.":
+            "Минимальный год, который можно ввести.",
+        "Maximum Year": "Максимальный год",
+        "The maximum year that can be entered.":
+            "Максимальный год, который можно ввести.",
+        "The placeholder text that will appear when Year field is empty.":
+            "Текст-заполнитель, который будет отображаться, когда поле 'Год' будет пустым.",
+        "Hide the Year part of the component.":
+            "Скрыть часть компонента, относящуюся к году.",
+        "Hide the Month part of the component.":
+            "Скрыть часть компонента, относящуюся к месяцу",
+        "Hide the Day part of the component.":
+            "Скрыть часть компонента, относящуюся к месяцу",
+        "Require Day": "Обязательный ввод дня",
+        "Require Month": "Обязательный ввод месяца",
+        "Require Year": "Обязательный ввод года",
+        "Maximum Day": "Максимальный день",
+        "Minimum Day ": "Минимальный день",
+        Questions: "Вопросы",
+        "The questions you would like to ask in this survey question.":
+            "Вопросы, которые вы хотели бы задать в этом опросе.",
+        "The values that can be selected per question. Example: 'Satisfied', 'Very Satisfied', etc.": `Значения, которые могут быть выбраны для каждого вопроса. Например: "Удовлетворен", "Очень удовлетворен" и т.д.`,
+        "HTML Tag": "HTML Тег",
+        Attributes: "Аттрибуты",
+        "The content of this HTML element.": "Содержимое этого HTML-элемента",
+        "The tag of this HTML element.": "Тег этого HTML-элемента.",
+        "The CSS class for this HTML element.":
+            "Класс CSS для этого HTML-элемента.",
+        "Rerender the field whenever a value on the form changes.":
+            "Повторно отображайте поле всякий раз, когда изменяется значение в форме.",
+        "The attributes for this HTML element. Only safe attributes are allowed, such as src, href, and title.":
+            "Атрибуты для этого HTML-элемента. Разрешены только безопасные атрибуты, такие как src, href и title.",
+        "Column Properties": "Свойства столбца",
+        "The size and width settings for each column. One row is equal to 12. (e.g., a row with two columns spanning the entire page should be 6 and 6)":
+            "Параметры размера и ширины для каждого столбца. Одна строка равна 12. (например, строка с двумя столбцами, занимающими всю страницу, должна быть 6 и 6)",
+        Collapsible: "Складной",
+        "If checked, this will turn this Panel into a collapsible panel.":
+            "Если этот флажок установлен, эта панель превратится в складную.",
+        "Label for Key column": "Метка для ключевого столбца",
+        "Provide a label text for Key column (otherwise 'Key' will be used)":
+            "Укажите текст метки для ключевого столбца (в противном случае будет использоваться 'Key').",
+        "Disable Adding / Removing Rows":
+            "Отключить добавление / удаление строк",
+        "Add Another Text": "Добавьте другой текст",
+        "Set the text of the Add Another button.":
+            "Установите текст для кнопки Добавить еще одну.",
+        "Conditional Add Button": "Кнопка условного добавления",
+        "Specify condition when Add Button should be displayed.":
+            "Укажите условие, при котором должна отображаться кнопка добавления.",
+        "Row CSS Class": "Класс Row CSS",
+        "CSS class to add to the edit row wrapper.":
+            "CSS-класс для добавления в оболочку редактирования строк.",
+        "Display as Modal": "Отображать как модальное окно",
+        "Display a modal to add or edit entries in the table":
+            "Отображение модального режима для добавления или редактирования записей в таблице",
+        "Save Row Text": "Сохранить текст строки",
+        "Set the text of the Save Row button.":
+            "Установите текст для кнопки Сохранить строку.",
+        "Remove Row Text": "Удалить текст строки",
+        "Set the text of the remove Row button.":
+            "Установите текст кнопки 'Удалить строку'.",
+        "Footer Template": "Шаблон нижнего колонтитула",
+        "This is the Lodash Template used to render the footer of the Edit grid.":
+            "Это шаблон Lodash, используемый для отображения нижнего колонтитула сетки редактирования.",
+        "Two available variables. 'value' is the array of row data and 'components' is the array of components in the grid.": `Это шаблон Lodash, используемый для отображения нижнего колонтитула сетки редактирования.`,
+        "Row Template": "Шаблон строки",
+        "This is the Lodash Template used to render each row of the Edit grid.":
+            "Это шаблон Lodash, используемый для отображения каждой строки таблицы редактирования.",
+        "Three available variables. 'row' is an object of one row's data, 'components' is the array of components in the grid and 'state' is current row's state (can be 'draft' or 'saved'). To add click events, add the classes 'editRow' and 'removeRow' to elements.": `Доступны три переменные. "строка" - это объект данных одной строки, "компоненты" - это массив компонентов в таблице, а "состояние" - это состояние текущей строки (может быть "черновик" или "сохранено"). Чтобы добавить события щелчка, добавьте классы "editRow" и "removeRow" к элементам.`,
+        "Equal column width": "Одинаковая ширина столбца",
+        "Enable Row Groups": "Включить группы строк",
+        "Initialize Empty": "Инициализировать пустым",
+        "The DataGrid will have no visible rows when initialized.":
+            "При инициализации в DataGrid не будет видимых строк.",
+        "Open First Row when Empty": "Открыть первый ряд, когда он пуст",
+        Templates: "Шаблоны",
+        "Header Template": "Шаблон заголовка",
+        "Allow Reorder": "Разрешить изменение порядка",
+        "Add Another Position": "Добавьте еще одну позиция",
+        "Position for Add Another button with respect to Data Grid Array.":
+            "Расположить кнопку добавления другой кнопки относительно массива таблицы данных.",
+
+        "This is the Lodash Template used to render the header of the Edit grid.":
+            "Это шаблон Lodash, используемый для отображения заголовка таблицы редактирования.",
     },
     en: {
         Button: "Button",
